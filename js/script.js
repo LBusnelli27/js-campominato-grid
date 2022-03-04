1e6dc5c1b44cb22912d7cba4c3d78aed30c7fb35
Busna27 <lucabusnelli2001@gmail.com>
2022-03-04 17:35:20 +0100

    first part of the game - done

--- js/script.js
+++ js/script.js
@@ -1,24 +1,31 @@
 // ********** FUNCTION **********
 
-const playGround = document.getElementById('my-play-groud');
-
+/**
+ * This function create a div with a class
+ * 
+ * @returns A div with class my-col
+ */
 function createGridSquare() {
     const currentElement = document.createElement('div')
     currentElement.classList.add('my-col')
-
+    
     return currentElement;
 }
 
-
+/**
+ * This function create n. boxes and add an eventListener
+ * 
+ * @param {*} nBox Number of boxes generated
+ */
 function createCol(nBox) {
     for(let i = 0; i < nBox; i++) {
         const currentSquare = createGridSquare();
         currentSquare.innerHTML = `${i + 1}`
-
+        
         currentSquare.addEventListener('click', function() {
             this.classList.add('clicked')
         })
-
+        
         playGround.appendChild(currentSquare);
     }
 }
@@ -26,4 +33,20 @@ function createCol(nBox) {
 // ********** END FUNCTION **********
 
 
-createCol(30);
+const playGround = document.getElementById('my-play-groud');
+
+const playBtn = document.getElementById('play-button');
+playBtn.addEventListener('click', function() {
+    console.log('Stai cliccando il bottone play!');
+    const difficultyChoise = document.getElementById('inputGroupSelect');
+    if (difficultyChoise.value == 1) {
+        createCol(100);
+    } else if (difficultyChoise.value == 2) {
+        createCol(80);
+    } else if (difficultyChoise.value == 3) {
+        createCol(50);
+    } else {
+        console.warn('Non hai creato un bel nulla!!!')
+    }
+})
+
