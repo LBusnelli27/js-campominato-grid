bcf567582f79fd31f9dcba915b60fc9c99d1a511
Busna27 <lucabusnelli2001@gmail.com>
2022-03-04 17:20:41 +0100

    create function that create blocks + add event

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
 // ********** FUNCTION **********
 
+const playGround = document.getElementById('my-play-groud');
+
+function createGridSquare() {
+    const currentElement = document.createElement('div')
+    currentElement.classList.add('my-col')
+
+    return currentElement;
+}
+
+
+function createCol(nBox) {
+    for(let i = 0; i < nBox; i++) {
+        const currentSquare = createGridSquare();
+        currentSquare.innerHTML = `${i + 1}`
+
+        currentSquare.addEventListener('click', function() {
+            this.classList.add('clicked')
+        })
+
+        playGround.appendChild(currentSquare);
+    }
+}
+
+// ********** END FUNCTION **********
+
+
+createCol(30);
